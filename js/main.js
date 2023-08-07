8288a85d4e751cb2b21c55de628305775688594c
newsks <sweetysks23@gmail.com>
2023-08-07 16:02:05 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 // Badge 스크롤JS & to-top 스크롤JS
 
 const badgeEl = document.querySelector('header .badges')
@@ -147,8 +131,4 @@ spyEls.forEach(function(spyEl){
     })
     .setClassToggle(spyEl, 'show')  
     .addTo(new ScrollMagic.Controller());
-});
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();  
+}); 
